354bd609248ad7b2a690bbae2219ccb6b26af4b4
usaone <uddipmitra@gmail.com>
2018-08-08 23:57:59 -0400

    Add user object with data

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
-function sayHello() {
-    console.log("Hello");
-}
-
-sayHello();
-
-var sayBye = function(name) {
-    console.log("Bye " + name);
-}
-
-sayBye("John");
-
-function product(a, b) {
-    return a*b;
-}
-
-console.log(product(3, 4));
-
-var multiply = function(a, b) {
-    return a*b;
-}
-
-console.log(multiply(3, 4));
+var user = {
+    name: "John",
+    age: 34,
+    hobby: "Soccer",
+    isMarried: false,
+};
