7884394449111400769b63681f469d0b98af92fe
usaone <uddipmitra@gmail.com>
2018-08-08 23:45:33 -0400

    Add more javascript exercises

---
+++ Sec10/exercise6-SOLUTIONS.js
@@ -0,0 +1,23 @@
+// using this array, 
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+// 1. Remove element from the array.
+array.shift(); //remove the first element
+array.pop(); //remove the last element
+
+// 2. Sort the array in order. 
+array.sort();
+
+// 3. Put "Kiwi" at the end of the array.
+array.push("Kiwi");
+
+// 4. Remove "Apples" from the array.
+array.splice(0, 1);
+
+// 5. Sort the array in reverse order. 
+array.reverse();
+
+// using this array, 
+// var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+// access "Oranges".
+array2[1][1][0];
