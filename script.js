697a38130c0e8117009f9e5193250d0e15639596
usaone <uddipmitra@gmail.com>
2018-08-09 00:10:48 -0400

    Demonstrate how Facebook basically works

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-var user = {
-    name: "John",
-    age: 34,
-    hobby: "Soccer",
-    isMarried: false,
-};
+var database = [
+    {
+        username: "andrei",
+        password: "super"
+    }
+];
+
+var newsFeed = [
+    {
+        username: "Bobby",
+        timeline: "So tired from all that learning!"
+    },
+    {
+        username: "Sally",
+        timeline: "Javascript is sooooo coool!"
+    }
+];
+
+var userNamePrompt = prompt("What is your username?");
+var passwordPrompt = prompt("What is your password?");
+
+function signIn(user, pass) {
+    if (database[0].username === user && database[0].password === pass)
+    {
+        console.log(newsFeed);
+    }
+    else
+    {
+        alert("Sorry, unknown username or password!");
+    }
+}
+
+signIn(userNamePrompt, passwordPrompt);
