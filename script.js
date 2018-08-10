a4362598f4d3b7801a626471900bce742534d086
usaone <uddipmitra@gmail.com>
2018-08-10 00:29:28 -0400

    Add click event to button

--- script.js
+++ script.js
@@ -1,30 +1,7 @@
-var todos = [
-    "clean room",
-    "brush teeth",
-    "exercise",
-    "study javascript",
-    "eat healthy"
-];
+var button = document.getElementsByTagName("button")[0];
 
-for (var i=0; i < todos.length; i++) {
-    console.log(todos[i]);
-}
-
-function logTodos(todo, i) {
-    console.log(todo, i);
-}
-
-todos.forEach(logTodos); //refer to CanIuse.com to see if this feature is available in all browsers or only specific browsers.
-
-var counterWhile = 0;
-while (counterWhile < 10) {
-    console.log(counterWhile);
-    counterWhile++;
-}
-
-var counterDo = 10;
-do {
-    console.log(counterDo);
-    counterDo--;
-} while (counterDo > 0);
+// Refer to https://developer.mozilla.org/en-US/docs/Web/Events for all the events
+button.addEventListener("click", function() {
+    console.log("CLICK!!!!");
+})
 
