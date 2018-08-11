57a6f6ab415403c576edca241fb112b1cccb47a5
usaone <uddipmitra@gmail.com>
2018-08-10 21:17:57 -0400

    Solution for DOM Events exercise in section 11 lecture 107

--- script.js
+++ script.js
@@ -1,7 +1,52 @@
-var button = document.getElementsByTagName("button")[0];
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
 
-// Refer to https://developer.mozilla.org/en-US/docs/Web/Events for all the events
-button.addEventListener("click", function() {
-    console.log("CLICK!!!!");
-})
+function inputLength() {
+    return input.value.length;
+}
 
+function createDelButton() {
+    var delButton = document.createElement("button");
+    delButton.appendChild(document.createTextNode("Delete"));
+
+    delButton.addEventListener("click", deleteListItemAfterClick);
+    return delButton;
+}
+
+function deleteListItemAfterClick(event) {
+    ul.removeChild(event.currentTarget.parentElement);
+}
+
+function createListElement() {
+    var li = document.createElement("li");
+    li.addEventListener("click", toggleListDone);
+    li.appendChild(document.createTextNode(input.value + " "));
+    li.appendChild(createDelButton());
+    ul.appendChild(li);
+    input.value = "";
+    input.focus();
+}
+
+function toggleListDone(event) {
+    event.currentTarget.classList.toggle("done");
+}
+
+
+function addListAfterClick() {
+    if (inputLength() > 0)
+    {
+        createListElement();
+    }
+}
+
+function addListAfterKeypress(event) {
+    if (inputLength() > 0 && event.keyCode == 13)
+    {
+        createListElement();
+    }
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
