523a5508f11509e61c5421e7d69cde264bc44ed4
usaone <uddipmitra@gmail.com>
2018-08-11 14:35:39 -0400

    Demonstrate default arguments

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ const age = 34;
 const pet = "horse";
 
 // Usually to construct a string we do
-const greeting = "Hello " + name + " you seem to be " + (age-10) + "!";
+// const greeting = "Hello " + name + " you seem to be " + (age-10) + "!";
 // Now we can do using something called Template Strings having back-ticks` 
-const greetingBest = `Hello ${name} you seem to be ${age-10}. What a lovely ${pet} you have`; 
+// const greetingBest = `Hello ${name} you seem to be ${age-10}. What a lovely ${pet} you have`; 
+
+// Default arguments
+function greet(name='', age=30, pet='cat') {
+    return `Hello ${name} you seem to be ${age-10}. What a lovely ${pet} you have`;
+}
+
+console.log(greet(name));
