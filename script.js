bb44508c875b95bf096f663641c49e8b488ecff7
usaone <uddipmitra@gmail.com>
2018-08-10 22:06:03 -0400

    Initial for new branch

--- script.js
+++ script.js
@@ -1,52 +1,9 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
 
-function inputLength() {
-    return input.value.length;
-}
+console.log(css);
+console.log(color1);
+console.log(color2);
 
-function createDelButton() {
-    var delButton = document.createElement("button");
-    delButton.appendChild(document.createTextNode("Delete"));
-
-    delButton.addEventListener("click", deleteListItemAfterClick);
-    return delButton;
-}
-
-function deleteListItemAfterClick(event) {
-    ul.removeChild(event.currentTarget.parentElement);
-}
-
-function createListElement() {
-    var li = document.createElement("li");
-    li.addEventListener("click", toggleListDone);
-    li.appendChild(document.createTextNode(input.value + " "));
-    li.appendChild(createDelButton());
-    ul.appendChild(li);
-    input.value = "";
-    input.focus();
-}
-
-function toggleListDone(event) {
-    event.currentTarget.classList.toggle("done");
-}
-
-
-function addListAfterClick() {
-    if (inputLength() > 0)
-    {
-        createListElement();
-    }
-}
-
-function addListAfterKeypress(event) {
-    if (inputLength() > 0 && event.keyCode == 13)
-    {
-        createListElement();
-    }
-}
-
-button.addEventListener("click", addListAfterClick);
-
-input.addEventListener("keypress", addListAfterKeypress);
+alert('test');
