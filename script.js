6bb811d9793d67f566d43f2dd1e9ef0ee9e26de2
usaone <uddipmitra@gmail.com>
2018-08-11 23:05:02 -0400

    Javascript Classes, Constructors and Methods, and Instantiation

---
+++ script.js
@@ -0,0 +1,28 @@
+// Instantiation
+
+class Player {
+    constructor(name, type) {
+        this.name = name;
+        this.type = type;
+    }
+    introduce() {
+        console.log(`Hi I am ${this.name}, I'm a ${this.type}`);
+    }
+}
+
+class Wizard extends Player {
+    constructor(name, type) {
+        super(name, type);
+    }
+    play() {
+        console.log(`WEEEEEEE I'm a ${this.type}`);
+    }
+}
+
+const wizard1 = new Wizard('Shelly', 'Healer');
+const wizard2 = new Wizard('Shawn', 'Dark Magic');
+
+wizard1.introduce();
+wizard2.introduce();
+wizard1.play();
+wizard2.play();
