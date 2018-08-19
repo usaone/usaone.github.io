04cddb07a75407aea0710bcb0a3088b696b8470e
usaone <uddipmitra@gmail.com>
2018-08-19 02:32:50 -0400

    Promise.all with setTimeout function

--- script.js
+++ script.js
@@ -1,61 +1,26 @@
+// Now again Promises from Andrei Neagoie's Udemy course
 
-// Learn about Promises in Javascript
-// From https://youtu.be/s6SH72uAn3Q
+const promise = new Promise((resolve, reject) => {
+  if (true) {
+    resolve('Stuff Worked');
+  } else {
+    reject('Error, it broke');
+  }
+});
 
-// Next level of understanding of promises
-// to show dependency of one action on another
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'HIII');
+});
 
-// let cleanRoom = function() {
-//   return new Promise(function(resolve, reject){
-//     resolve('cleaned the room');
-//   });
-// };
-let cleanRoom = function() {
-  return new Promise((resolve, reject) => resolve('cleaned the room'));
-};
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 2000, 'POOKIE');
+});
 
+const promise4 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 4000, 'Is it me you are looking for?');
+});
 
-// let removeGarbage = function(message) {
-//   return new Promise(function(resolve, reject){
-//     resolve(message + ' and removed the garbage');
-//   });
-// };
-let removeGarbage = function(message) {
-  return new Promise((resolve, reject) => resolve(message + ' and removed the garbage'));
-};
-
-// let winIcecream = function(message) {
-//   return new Promise(function(resolve, reject){
-//     resolve(message + ' and was rewarded with ice cream.');
-//   });
-// };
-let winIcecream = function(message) {
-  return new Promise((resolve, reject) => resolve(message + ' and was rewarded with ice cream.'));
-};
-
-// we will call cleanRoom() which will return 
-// a Promise as defined above.
-// the first .then() method will be called when 
-// resolve executes in the cleanRoom promise and 
-// returns the removeGarbage promise.
-// the second .then() method runs on the successful
-// execution of the removeGarbage promise resolve method
-// and that returns the winIcecream promise.
-cleanRoom().then(result => removeGarbage(result))
-.then(result => winIcecream(result))
-.then(result => console.log('Done! I ' + result));
-
-// If you want to do all three and then after
-// completion of all, do something else, then
-// you would do
-Promise.all([cleanRoom(), removeGarbage(), winIcecream()])
-.then(() => console.log('All done'));
-
-Promise.race([cleanRoom(), removeGarbage(), winIcecream()])
-.then(() => console.log('One done! Two more to go.'));
-
-//------------------------------------------
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => console.log(JSON.stringify(json)));
-  
+Promise.all([promise, promise2, promise3, promise4])
+.then(values => {
+  console.log(values);
+});
