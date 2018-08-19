6a818315e802a8d856b5f3b7b09fab0db6aa4af8
usaone <uddipmitra@gmail.com>
2018-08-19 19:03:34 -0400

    Async Await and exercises

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
-// Now again Promises from Andrei Neagoie's Udemy course
+async function fetchUsers() {
+  const resp = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await resp.json();
+  console.log(data);
+}
+
+// fetchUsers();
 
 const urls = [
-'http://jsonplaceholder.typicode.com/users',
-'http://jsonplaceholder.typicode.com/posts',
-'http://jsonplaceholder.typicode.com/albums'
+  'https://jsonplaceholder.typicode.com/users',
+  'https://jsonplaceholder.typicode.com/posts',
+  'https://jsonplaceholder.typicode.com/albums'
 ];
 
-Promise.all(urls.map(url => {
-  return fetch(url).then(resp => resp.json())
-}))
-.then(results => {
-  console.log(results[0]);
-  console.log(results[1]);
-  console.log(results[2]);
-})
-.catch(() => console.log('Error!'));
+// Promise.all(urls.map(url =>
+//   fetch(url).then(resp => resp.json())
+// ))
+// .then(array => {
+//   console.log('users(old)', array[0]);
+//   console.log('posts(old)', array[1]);
+//   console.log('albums(old)', array[2]);
+// })
+// .catch(err => console.log('ughhhh fix it!', err));
+
+const getData = async function() {
+  try {
+    const [users, posts, albums] = await Promise.all(urls.map(url => fetch(url).then(resp => resp.json())));
+  
+    console.log('users', users);
+    console.log('posts', posts);
+    console.log('albums', albums);
+  }
+  catch(err) {
+    console.log('Error Encountered');
+    console.log(err);
+    console.log('Error Encountered');
+  }
+};
+
+getData();
