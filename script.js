282d22621fe7a6f87d014913a6a2a3499486e9eb
usaone <uddipmitra@gmail.com>
2018-08-19 02:41:52 -0400

    Promises in practical use

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
 // Now again Promises from Andrei Neagoie's Udemy course
 
-const promise = new Promise((resolve, reject) => {
-  if (true) {
-    resolve('Stuff Worked');
-  } else {
-    reject('Error, it broke');
-  }
-});
+const urls = [
+'http://jsonplaceholder.typicode.com/users',
+'http://jsonplaceholder.typicode.com/posts',
+'http://jsonplaceholder.typicode.com/albums'
+];
 
-const promise2 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 100, 'HIII');
-});
-
-const promise3 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 2000, 'POOKIE');
-});
-
-const promise4 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 4000, 'Is it me you are looking for?');
-});
-
-Promise.all([promise, promise2, promise3, promise4])
-.then(values => {
-  console.log(values);
-});
+Promise.all(urls.map(url => {
+  return fetch(url).then(resp => resp.json())
+}))
+.then(results => {
+  console.log(results[0]);
+  console.log(results[1]);
+  console.log(results[2]);
+})
+.catch(() => console.log('Error!'));
